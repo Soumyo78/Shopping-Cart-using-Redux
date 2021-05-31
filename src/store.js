f4b9a71816ca8981ceb1ba6cf5891729b2d2b7e3
Soumyo78 <soumyo78@yahoo.com>
2021-06-01 00:59:45 +0530

    Redux Dev Tools has been integrated for this project

--- src/store.js
+++ src/store.js
@@ -3,7 +3,10 @@ import cartReducer from "./reducers/cartReducer";
 
 // Store Creator Function
 const storeCreator = () => {
-  return createStore(cartReducer);
+  return createStore(
+    cartReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 };
 
 export default storeCreator;
